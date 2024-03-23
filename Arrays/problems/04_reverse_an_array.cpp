f0d3570cfa6689915975c7df773c95ea45935ba1
Subramanya12244 <asubramanya370@gmail.com>
2024-03-24 03:03:52 +0530

    commit

---
+++ Arrays/problems/04_reverse_an_array.cpp
@@ -0,0 +1,35 @@
+using namespace std;
+#include <iostream>
+void printing(int arr[],int size);
+void reverse(int arr[],int size)
+{
+    int start=0;
+    int end=size-1;
+    while(start<end)
+    {
+        int temp=arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+    printing(arr,size);
+}
+void printing(int arr[],int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" "<<endl;
+    }
+}
+int main()
+{
+    int n;
+    cin >>n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    reverse(arr,n);
+}
---
+++ Arrays/problems/05_extreme.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+#include <iostream>
+
+void extreme(int arr[],int size)
+{
+    int start=0;
+    int end=size-1;
+    while(start<end)
+    {
+        cout<<arr[start]<<endl;
+        cout<<arr[end]<<endl;
+        start++;
+        end--;
+    }
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    extreme(arr, n);
+}
