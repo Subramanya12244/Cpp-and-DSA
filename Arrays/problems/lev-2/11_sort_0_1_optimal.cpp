323312cdb92b329b628fbcf35769eceb1b0ec718
Subramanya12244 <asubramanya370@gmail.com>
2024-03-25 16:11:46 +0530

    commit

--- Arrays/problems/lev-2/11_sort_0_1_optimal.cpp
+++ Arrays/problems/lev-2/11_sort_0_1_optimal.cpp
@@ -27,6 +27,33 @@ int optimal(vector<int> &arr,int n)
         cout<<arr[k];
     }
 }
+int optimal_another_approach(vector<int> &arr,int n)
+{
+    int i=0;
+    int start=0;
+    int end=arr.size()-1;
+
+    while(start<end)
+    {
+        if(arr[i]==0)
+        {
+            swap(arr[i],arr[start]);
+            i++;
+            start++;
+        }
+        if(arr[i]==1)
+        {
+            swap(arr[i],arr[end]);
+            i++;
+            end--;
+            
+        }
+    }
+    for(int k=0;k<n;k++)
+    {
+        cout<<arr[k];
+    }
+}
 int main()
 {
     int n;
@@ -36,5 +63,6 @@ int main()
     {
       cin>>arr[i];  
     }
-optimal(arr,n);
+// optimal(arr,n);
+optimal_another_approach(arr,n);
 }
--- Arrays/problems/lev-3/13_Union_of_2_arrays.cpp
+++ Arrays/problems/lev-3/13_Union_of_2_arrays.cpp
@@ -17,6 +17,36 @@ int Union_no_dup(vector<int>a, vector<int>b)
         cout<<ans[i]<<endl;
     }
 }
+
+int Union_with_dup(vector<int>a, vector<int>b)
+{
+    vector<int> ans;
+    for(int i=0; i<a.size();i++)
+    {
+        for(int j=0; j<b.size();j++)
+        {
+            if(a[i]==b[j])
+            {
+                b[j]=-1;
+            }
+        }
+    }
+    for(int i=0; i<a.size();i++)
+    {
+        ans.push_back(a[i]);
+    }
+     for(int i=0; i<b.size();i++)
+    {
+        if(b[i]!=-1)
+        {
+            ans.push_back(b[i]);
+        }
+    }
+    for(int i=0; i<ans.size();i++)
+    {
+        cout<<ans[i]<<endl;
+    }
+}
 int main()
 {
     int m;
@@ -38,5 +68,6 @@ int main()
     {
         cin>>b[i];
     }
-    Union_no_dup(a,b);
+    // Union_no_dup(a,b);
+    Union_with_dup(a,b);
 }
