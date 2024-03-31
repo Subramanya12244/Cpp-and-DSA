23bf68a7c4638cd061a3036b1eb4b801602eebd4
Subramanya12244 <asubramanya370@gmail.com>
2024-03-31 12:39:01 +0530

    commit

--- Arrays/1-D Arrays/problems/leet code/05_move_all_negative_to_the_left.cpp
+++ Arrays/1-D Arrays/problems/leet code/05_move_all_negative_to_the_left.cpp
@@ -1,1 +1,38 @@
 using namespace std;
+#include<iostream>
+#include<vector>
+int move_negative(vector<int> arr)
+{
+    int low=0;
+    int high=arr.size()-1;
+    while(low<high)
+    {
+        if(arr[low]<0)
+        {
+            low++;
+        }
+        else if(arr[high]>0)
+        {
+            high--;
+        }
+        else{
+            swap(arr[high],arr[low]);
+        }
+    }
+    for(int i=0; i<arr.size(); i++)
+{
+    cout<<arr[i]<<endl;
+}
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+move_negative(arr);
+
+}
