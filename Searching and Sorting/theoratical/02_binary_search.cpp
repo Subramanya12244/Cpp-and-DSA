4cc8bf1f6a3921e97cfa40ec73232b08675bc6fb
Subramanya12244 <asubramanya370@gmail.com>
2024-04-03 13:18:30 +0530

    commit

--- Searching and Sorting/theoratical/02_binary_search.cpp
+++ Searching and Sorting/theoratical/02_binary_search.cpp
@@ -1,10 +1,28 @@
 using namespace std;
 #include<iostream>
 #include<vector>
-
+// it is the second searching algorithm for searching it is better than the linear search algorithm which is more efficient as its time complexity is O(log(n)) where n is the size of the array
 int binarySearch(vector<int> arr,int target)
 {
-    
+    int start=0;
+    int end=arr.size()-1;
+    while(start<end)
+    {
+        int mid=start+(end-start)/2;
+        if(arr[mid]==target)
+            return mid;
+        else if(arr[mid]<target)
+        {
+            // search in rightside of mid
+            start=mid+1;
+        }
+        else{
+            // search in leftside of mid
+            end=mid-1;
+        }
+
+    }
+    return -1;
 }
 int main()
 {
@@ -18,4 +36,6 @@ int main()
     int target;
     cout<<"enter the target";
     cin>>target;
+
+    cout<<binarySearch(arr,target);
 }
